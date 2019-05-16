78f77563a0f2b1ffe4d5638e062dcce1ad81ce36
zolkiss <kisszoltan89@gmail.com>
2019-05-16 19:35:38 +0200

    Creating page builder base

--- src/main/java/hu/zolkiss/vaadinuibuildersample/VaadinUiBuilderSampleApplication.java
+++ src/main/java/hu/zolkiss/vaadinuibuildersample/VaadinUiBuilderSampleApplication.java
@@ -2,12 +2,14 @@ package hu.zolkiss.vaadinuibuildersample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"hu.zolkiss.vaadinuibuildersample", "hu.zolkiss.uibuilder"})
 public class VaadinUiBuilderSampleApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(VaadinUiBuilderSampleApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(VaadinUiBuilderSampleApplication.class, args);
+  }
 
 }
