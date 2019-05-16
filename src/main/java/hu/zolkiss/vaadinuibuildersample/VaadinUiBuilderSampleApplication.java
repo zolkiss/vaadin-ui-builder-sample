65361f5926d2c0f3403d023dfc89ede4ff375b6f
=zolkiss <kisszoltan89@gmail.com>
2019-05-16 07:55:15 +0200

    Sample project init

---
+++ src/main/java/hu/zolkiss/vaadinuibuildersample/VaadinUiBuilderSampleApplication.java
@@ -0,0 +1,13 @@
+package hu.zolkiss.vaadinuibuildersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaadinUiBuilderSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaadinUiBuilderSampleApplication.class, args);
+    }
+
+}
